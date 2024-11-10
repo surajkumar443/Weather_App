ddea384695ee06cf5e0a0bbc3465504285eb6ba3
surajkumar443 <surajs4932@gmail.com>
2024-11-11 02:48:16 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "c5852d6a03ff67adc37c8449a3551afb",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
